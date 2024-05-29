4d4eb4dd5b53cadf22d783d902e149075298e51c
Tenzin Kalden <tenkalden007@gmail.com>
2024-05-29 11:18:20 +0530

    update module

--- tools/pipeline_for_images_download.py
+++ tools/pipeline_for_images_download.py
@@ -1,15 +1,58 @@
 import os
 import re
+import configparser
+import boto3
 import random
 from pathlib import Path
-from utils import get_hash, is_archived
+from tools.utils import get_hash, is_archived
 from openpecha.buda.api import get_buda_scan_info, get_image_list
-from config import BDRC_ARCHIVE_BUCKET as bucket_name, bdrc_archive_s3_client as s3_client
+from tools.config import BDRC_ARCHIVE_BUCKET as bucket_name, bdrc_archive_s3_client as s3_client
+
+BDRC_ARCHIVE_BUCKET = "archive.tbrc.org"
+
+aws_credentials_file = os.path.expanduser("~/.aws/credentials")
+config = configparser.ConfigParser()
+config.read(aws_credentials_file)
+
+
+bdrc_archive_session = boto3.Session(
+    aws_access_key_id= config.get("archive_tbrc_org", "aws_access_key_id"),
+    aws_secret_access_key= config.get("archive_tbrc_org", "aws_secret_access_key")
+)
+bdrc_archive_s3_client = bdrc_archive_session.client('s3')
+bdrc_archive_s3_resource = bdrc_archive_session.resource('s3')
+bdrc_archive_bucket = bdrc_archive_s3_resource.Bucket(BDRC_ARCHIVE_BUCKET)
+
+
+bucket_name = BDRC_ARCHIVE_BUCKET
+s3 = bdrc_archive_s3_client
+
+
+def download_and_save_image(bucket_name, obj_dict, save_path):
+    if obj_dict == None:
+        return
+    for _, obj_keys in obj_dict.items():
+        for obj_key in obj_keys:
+            image_name = obj_key.split("/")[-1]
+            image_path = Path(f"{save_path}/{image_name}")
+            if image_path.exists():
+                continue
+            try:
+                response = s3.get_object(Bucket=bucket_name, Key=obj_key)
+                image_data = response['Body'].read()
+                
+                with open(image_path, 'wb') as f:
+                    f.write(image_data)
+                
+                print(f"Image downloaded and saved as {save_path}")
+            except Exception as e:
+                print(f"Error: {e}")
+
 
 def remove_non_page(images_list, work_id, image_group_id):
     s3_keys = []
     hash_two = get_hash(work_id)
-    if not re.match(r"[A-Z]", image_group_id[1:]):
+    if bool(re.match(r"[A-Z]", image_group_id[1:])) == False:
         image_group_id = image_group_id[1:]
     for image in images_list:
         if int(image['filename'].split(".")[-0][-3:]) <= 5:
@@ -19,7 +62,8 @@ def remove_non_page(images_list, work_id, image_group_id):
             s3_keys.append(s3_key)
     return s3_keys
 
-def get_random_images(work_id, s3_client, bucket_name, random_flag=True):
+
+def get_random_images_dict(work_id, s3_client, bucket_name, random_flag=True):
     final_dict = {}
     curr_dict = {}
     scan_info = get_buda_scan_info(work_id)
@@ -50,32 +94,13 @@ def get_random_images(work_id, s3_client, bucket_name, random_flag=True):
         curr_dict[image_group_id] = images_s3_keys
         final_dict.update(curr_dict)
     return final_dict
-
-def download_and_save_image(bucket_name, obj_dict, save_path):
-    if obj_dict is None:
-        return
-    for _, obj_keys in obj_dict.items():
-        for obj_key in obj_keys:
-            image_name = obj_key.split("/")[-1]
-            image_path = Path(f"{save_path}/{image_name}")
-            if image_path.exists():
-                continue
-            try:
-                print(f"Verifying s3_key: {obj_key}") 
-                response = s3_client.get_object(Bucket=bucket_name, Key=obj_key)
-                image_data = response['Body'].read()
-                with open(image_path, 'wb') as f:
-                    f.write(image_data)
-                print(f"Image downloaded and saved as {image_path}")
-            except Exception as e:
-                print(f"Error downloading {obj_key}: {e}") 
-
+        
 def main():
     work_ids = Path(f"../data/work_ids/derge_works.txt").read_text(encoding='utf-8').split("\n")
-    for work_id in work_ids:
+    for work_id in work_ids[88:]:
         save_path = Path(f'../data/images/{work_id}')
         save_path.mkdir(exist_ok=True, parents=True)
-        images_dict = get_random_images(work_id, s3_client, bucket_name, random_flag=False)
+        images_dict = get_random_images_dict(work_id, s3_client, bucket_name, random_flag=False)
         download_and_save_image(bucket_name, images_dict, save_path)
 
 if __name__ == "__main__":
