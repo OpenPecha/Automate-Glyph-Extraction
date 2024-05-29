47d6d5e8877f9c2560e017bb520e2bf1ce8211f1
Tenzin Kalden <tenkalden007@gmail.com>
2024-05-29 10:47:03 +0530

    for debugging

--- tools/config.py
+++ tools/config.py
@@ -5,14 +5,30 @@ import boto3
 BDRC_ARCHIVE_BUCKET = "archive.tbrc.org"
 
 aws_credentials_file = os.path.expanduser("~/.aws/credentials")
+
 config = configparser.ConfigParser()
 config.read(aws_credentials_file)
 
+
+if "archive_tbrc_org" not in config:
+    print("Error: 'archive_tbrc_org' section not found in AWS credentials file.")
+    exit(1)
+
+if "aws_access_key_id" not in config["archive_tbrc_org"]:
+    print("Error: 'aws_access_key_id' not found in 'archive_tbrc_org' section.")
+    exit(1)
+
+if "aws_secret_access_key" not in config["archive_tbrc_org"]:
+    print("Error: 'aws_secret_access_key' not found in 'archive_tbrc_org' section.")
+    exit(1)
+    
 bdrc_archive_session = boto3.Session(
-    aws_access_key_id=config.get("archive_tbrc_org", "aws_access_key_id"),
-    aws_secret_access_key=config.get("archive_tbrc_org", "aws_secret_access_key")
+    aws_access_key_id=config["archive_tbrc_org"]["aws_access_key_id"],
+    aws_secret_access_key=config["archive_tbrc_org"]["aws_secret_access_key"]
 )
+
 bdrc_archive_s3_client = bdrc_archive_session.client('s3')
 bdrc_archive_s3_resource = bdrc_archive_session.resource('s3')
+
 bdrc_archive_bucket = bdrc_archive_s3_resource.Bucket(BDRC_ARCHIVE_BUCKET)
 ocr_output_bucket = bdrc_archive_s3_resource.Bucket("ocr.bdrc.io")
