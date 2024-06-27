910b232cee6a6a705e8a5e95da05a0ccdba7e548
tenkal <tenkalden007@gmail.com>
2024-06-27 16:09:07 +0530

    updated scripts

--- tools/download.py
+++ tools/download.py
@@ -1,7 +1,7 @@
 import hashlib
 import json
+import boto3
 from pathlib import Path
-from config import bdrc_archive_s3_client as s3_client 
 
 def get_hash(work_id):
     md5 = hashlib.md5(str.encode(work_id))
@@ -11,16 +11,21 @@ def get_hash(work_id):
 def download_images_from_s3(work_id, json_file, bucket_name, download_dir):
     with open(json_file, 'r', encoding='utf-8') as file:
         data = json.load(file)
-
-    s3 = s3_client  
+        
+    s3 = boto3.client('s3')
     hash_two = get_hash(work_id)
     for entry in data:
         image_group_id = entry['image_group_id']
+        
+        # Modify image_group_id if necessary
+        if not (image_group_id[2].isalpha() or image_group_id[3].isalpha()):
+            image_group_id = image_group_id[1:]
+        
         for image_filename in entry['references']:
             s3_key = f"Works/{hash_two}/{work_id}/images/{work_id}-{image_group_id}/{image_filename}"
-
+        
             try:
-                download_path = Path(download_dir) / image_filename
+                download_path = Path(download_dir) / f"{work_id}-{image_group_id}-{image_filename}"
                 download_path.parent.mkdir(parents=True, exist_ok=True)
                 
                 s3.download_file(bucket_name, s3_key, str(download_path))
@@ -37,3 +42,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
